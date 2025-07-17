f036b11f8faea7ff5041efbb380deebc6e8f955d
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-07-17 11:39:06 +0200

    verify on OrderService

--- src/test/java/demo/application/web/OrderControllerMockMvcTest.java
+++ src/test/java/demo/application/web/OrderControllerMockMvcTest.java
@@ -2,6 +2,9 @@ package demo.application.web;
 
 import static java.lang.String.format;
 import static java.util.stream.Collectors.joining;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,6 +14,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.SignatureException;
 import java.util.Map;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -30,6 +35,9 @@ import demo.application.TestPublicKeyResolver;
 import demo.application.crypto.PublicKeyResolver;
 import demo.application.cyrpto.KeyGenerator;
 import demo.application.cyrpto.PayloadSigner;
+import demo.application.domain.Order;
+import demo.application.domain.Order.Price;
+import demo.application.domain.OrderService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -50,10 +58,13 @@ class OrderControllerMockMvcTest {
 	@Autowired
 	MockMvc mockMvc;
 
+	@MockitoBean
+	OrderService orderService;
+
 	@Test
 	void accepts() throws Exception {
-		var id = "3aebf66c-d8e5-456a-887c-53e5fc45f0a1";
-		var payload = """
+		var id = UUID.fromString("3aebf66c-d8e5-456a-887c-53e5fc45f0a1");
+		var order = """
 				{
 					"id": "%s",
 					"price": {
@@ -62,17 +73,19 @@ class OrderControllerMockMvcTest {
 					}
 				}
 				""".formatted(id);
-		var payloadWithSignature = payloadWithSignature(payload, Config.keyPair.toPrivateKey());
+		var payloadWithSignature = payloadWithSignature(order, Config.keyPair.toPrivateKey());
 		mockMvc.perform(put("/orders/%s".formatted(id)) //
 				.contentType(APPLICATION_JSON) //
 				.content(payloadWithSignature)) //
 				.andExpect(status().isOk());
+		verify(orderService).create(id, new Order(id, new Price(1.99, "EUR")));
+		verifyNoMoreInteractions(orderService);
 	}
 
 	@Test
 	void acceptsReformatted() throws Exception {
-		var id = "3aebf66c-d8e5-456a-887c-53e5fc45f0a1";
-		var payload = """
+		var id = UUID.fromString("3aebf66c-d8e5-456a-887c-53e5fc45f0a1");
+		var order = """
 				{
 					"id": "%s",
 					"price": {
@@ -81,19 +94,21 @@ class OrderControllerMockMvcTest {
 					}
 				}
 				""".formatted(id);
-		var payloadWithSignature = payloadWithSignature(payload, Config.keyPair.toPrivateKey());
+		var payloadWithSignature = payloadWithSignature(order, Config.keyPair.toPrivateKey());
 		var reformattedJson = payloadWithSignature.lines().map(l -> format("   \t  %s \t  ", l)).collect(joining("\n"));
 		assert !payloadWithSignature.equals(reformattedJson);
 		mockMvc.perform(put("/orders/%s".formatted(id)) //
 				.contentType(APPLICATION_JSON) //
 				.content(reformattedJson)) //
 				.andExpect(status().isOk());
+		verify(orderService).create(id, new Order(id, new Price(1.99, "EUR")));
+		verifyNoMoreInteractions(orderService);
 	}
 
 	@Test
 	void refusesBecauseModified() throws Exception {
 		var id = "3aebf66c-d8e5-456a-887c-53e5fc45f0a1";
-		var payload = """
+		var order = """
 				{
 					"id": "%s",
 					"price": {
@@ -102,19 +117,20 @@ class OrderControllerMockMvcTest {
 					}
 				}
 				""".formatted(id);
-		var payloadWithSignature = payloadWithSignature(payload, Config.keyPair.toPrivateKey());
+		var payloadWithSignature = payloadWithSignature(order, Config.keyPair.toPrivateKey());
 		var manipulatedJson = manipulateJson(payloadWithSignature);
 
 		mockMvc.perform(put("/orders/%s".formatted(id)) //
 				.contentType(APPLICATION_JSON) //
 				.content(manipulatedJson)) //
 				.andExpect(status().isForbidden());
+		verifyNoInteractions(orderService);
 	}
 
 	@Test
 	void refusesBecauseSignedByUnknownKey() throws Exception {
 		var id = "3aebf66c-d8e5-456a-887c-53e5fc45f0a1";
-		var payload = """
+		var order = """
 				{
 					"id": "%s",
 					"price": {
@@ -125,13 +141,14 @@ class OrderControllerMockMvcTest {
 				""".formatted(id);
 		PrivateKey otherPrivateKey = KeyGenerator.newRandomRsaKeyPair(Config.keyPair.getKeyID() + "-another")
 				.toPrivateKey();
-		var payloadWithSignature = payloadWithSignature(payload, otherPrivateKey);
+		var payloadWithSignature = payloadWithSignature(order, otherPrivateKey);
 		var manipulatedJson = manipulateJson(payloadWithSignature);
 
 		mockMvc.perform(put("/orders/%s".formatted(id)) //
 				.contentType(APPLICATION_JSON) //
 				.content(manipulatedJson)) //
 				.andExpect(status().isForbidden());
+		verifyNoInteractions(orderService);
 	}
 
 	@SuppressWarnings("unchecked")
